cea7d2466babee2b35368f943c2cb4309932f40b
WEIS <marcel.weissgerber@live.de>
2015-04-10 14:13:15 +0200

    test for commit

--- app/src/main/java/com/sim/star/bitworxx/starcity/constants/ColorSetter.java
+++ app/src/main/java/com/sim/star/bitworxx/starcity/constants/ColorSetter.java
@@ -7,11 +7,11 @@ import android.graphics.Color;
  */
 public class ColorSetter {
 
-    public static int FILL_BACK_COLOR = ColorMenu.FILL_YELLOW;
-    public static int FILL_FORE_COLOR = ColorMenu.FILL_YELLOW_FORE;
-    public static int FILL_BACK_COLOR_PLATE = ColorMenu.FILL_YELLOW_PLATE;
-    public static int FILL_STROKE_FORE = ColorMenu.FILL_YELLOW_PLATE;
-    public static int FILL_BACK_COLOR_PLATE_HALF = ColorMenu.FILL_YELLOW_PLATE_HALF;
+    public static int FILL_BACK_COLOR = ColorMenu.FILL_GREEN;
+    public static int FILL_FORE_COLOR = ColorMenu.FILL_GREEN_FORE;
+    public static int FILL_BACK_COLOR_PLATE = ColorMenu.FILL_GREEN_PLATE;
+    public static int FILL_STROKE_FORE = ColorMenu.FILL_GREEN_PLATE;
+    public static int FILL_BACK_COLOR_PLATE_HALF = ColorMenu.FILL_GREEN_PLATE_HALF;
 
     public static int FILL_STROKE_BACK = Color.argb(255, 0, 0, 0);
 
